1704379ab602f3b68bb2c90970f7aa9f099c0a42
Kenta Shinozaki <kurometaro331@gmail.com>
2021-07-06 15:31:11 +0900

    typescript, reactを導入。開発環境でブラウザにタイトル表示確認。

---
+++ src/pages/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Container, Typography } from '@material-ui/core'
+
+const App = () => {
+  return (
+    <Container style={{ marginTop: 10 }}>
+      <Typography variant='h5' component='h1' style={{ marginBottom: 20 }}>
+        Exam 2021.07 / Logical Fabrics
+      </Typography>
+    </Container>
+  )
+}
+
+export default App;
